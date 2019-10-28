a16b17f48c41a8ffebba016fb224816792c773f6
Joseph <dabigjoe6@gmail.com>
2019-10-28 22:19:44 +0100

    Created data of Todos

---
+++ src/data.ts
@@ -0,0 +1,22 @@
+let data = [
+	{
+		task: 'Trip to Paris',
+		todos: [
+			{ task: 'Book flights', done: true },
+			{ task: 'Passport check', done: true },
+			{ task: 'Packing luggage', done: false },
+			{ task: 'Hotel Reservation', done: false }
+		]
+	},
+	{
+		task: 'My Tasks',
+		todos: [
+			{ task: 'Buy milk', done: false },
+			{ task: 'Plan weekend', done: false },
+			{ task: 'Publish Friday', done: true },
+			{ task: 'Run 3 miles', done: false }
+		]
+	}
+]
+
+export default data 
